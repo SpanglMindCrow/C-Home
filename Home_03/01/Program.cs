f9418407a04008b54bcbe1c13c4ffaa9b0e32b10
SpanglMindCrow <spangl.mind.crow@gmail.com>
2022-04-22 21:37:42 +0500

    add Home_03

---
+++ Home_03/01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int a1 = N/10000;
+double a2 = (N/1000)%10;
+
+double b1 = N%10;
+double b2 = (N%100)/10;
+
+if ((a1 == b1) && (a2 ==b2))
+{
+    Console.WriteLine($"{N} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"{N} не является палиндромом");
+}
---
+++ Home_03/02/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84;  A (7,-5, 0); B (1,-1,9) -> 11.53;
+
+Console.WriteLine("Введите координату x первой точки");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y первой точки");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z первой точки");
+double zA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координату x второй точки");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату y второй точки");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату z второй точки");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+double x=(xA-xB)*(xA-xB);
+double y=(yA-yB)*(yA-yB);
+double z=(zA-zB)*(zA-zB);
+
+double c = Math.Sqrt(x + y + z);
+
+Console.WriteLine($"Расстояние между точками равно {c}");
